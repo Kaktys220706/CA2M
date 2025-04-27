c015babdd10b6e540106dedaf8cd2f586ddbf758
Kaktys220706 <150801504+Kaktys220706@users.noreply.github.com>
2025-04-27 23:38:31 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,8 @@
+
+function revealFile(id) {
+  document.getElementById(id).style.display = 'block';
+}
+function decrypt() {
+  document.getElementById('decryptedMsg').innerText = "THE CODE IS: SECRET LOCATION - PARIS";
+}
+
